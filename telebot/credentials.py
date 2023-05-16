236c72edb57b081dc27e9bbf21f2c3b3bd734c12
Tomiwa Ademidun <tomiademidun@gmail.com>
2023-05-16 15:23:39 +0200

    add gunicorn

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,3 @@
-bot_token = "here goes your access token from BotFather"
+bot_token = os.getenv('BOT_TOKEN', False)
 bot_user_name = "uslugebot"
 URL = "the heroku app link that we will create later"
