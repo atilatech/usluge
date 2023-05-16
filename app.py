027eedacb84eea6bd1adf0ddde06811f452853a7
Tomiwa Ademidun <tomiademidun@gmail.com>
2023-05-16 16:23:45 +0200

    reformat file

--- app.py
+++ app.py
@@ -8,53 +8,56 @@ global TOKEN
 TOKEN = bot_token
 bot = telegram.Bot(token=TOKEN)
 
-
 # start the flask app
 app = Flask(__name__)
 
+
 @app.route('/{}'.format(TOKEN), methods=['POST'])
 def respond():
-   # retrieve the message in JSON and then transform it to Telegram object
-   update = telegram.Update.de_json(request.get_json(force=True), bot)
+    # retrieve the message in JSON and then transform it to Telegram object
+    update = telegram.Update.de_json(request.get_json(force=True), bot)
 
-   chat_id = update.message.chat.id
-   msg_id = update.message.message_id
+    chat_id = update.message.chat.id
+    msg_id = update.message.message_id
 
-   # Telegram understands UTF-8, so encode text for unicode compatibility
-   text = update.message.text.encode('utf-8').decode()
-   print("got text message :", text)
-   # for debugging purposes only
-   print("got text message :", text)
-   print("chat id :", chat_id)
-   print("message id :", msg_id)
-   # the first time you chat with the bot AKA the welcoming message
-   if text == "/start":
-       # print the welcoming message
-       bot_welcome = """
+    # Telegram understands UTF-8, so encode text for unicode compatibility
+    text = update.message.text.encode('utf-8').decode()
+    print("got text message :", text)
+    # for debugging purposes only
+    print("got text message :", text)
+    print("chat id :", chat_id)
+    print("message id :", msg_id)
+    # the first time you chat with the bot AKA the welcoming message
+    if text == "/start":
+        # print the welcoming message
+        bot_welcome = """
        Welcome to Usluge, a bot for booking service providers in Montenegro.
        Start your sentence with 'book' to find a service provider.
        If you need help, send /help or message @@IvanKapisoda.
        """
-       # send the welcoming message
-       bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
+        # send the welcoming message
+        bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
 
-   elif text.startswith("book"):
-       response = find_service_provider(text)
-       bot.sendMessage(chat_id=chat_id, text=response, reply_to_message_id=msg_id)
-   else:
-       try:
-           # clear the message we got from any non alphabets
-           text = re.sub(r"\W", "_", text)
-           # create the api link for the avatar based on http://avatars.adorable.io/
-           url = "https://api.adorable.io/avatars/285/{}.png".format(text.strip())
-           # reply with a photo to the name the user sent,
-           # note that you can send photos by url and telegram will fetch it for you
-           bot.sendPhoto(chat_id=chat_id, photo=url, reply_to_message_id=msg_id)
-       except Exception:
-           # if things went wrong
-           bot.sendMessage(chat_id=chat_id, text="There was a problem in the name you used, please enter different name", reply_to_message_id=msg_id)
+    elif text.startswith("book"):
+        response = find_service_provider(text)
+        bot.sendMessage(chat_id=chat_id, text=response, reply_to_message_id=msg_id)
+    else:
+        try:
+            # clear the message we got from any non alphabets
+            text = re.sub(r"\W", "_", text)
+            # create the api link for the avatar based on http://avatars.adorable.io/
+            url = "https://api.adorable.io/avatars/285/{}.png".format(text.strip())
+            # reply with a photo to the name the user sent,
+            # note that you can send photos by url and telegram will fetch it for you
+            bot.sendPhoto(chat_id=chat_id, photo=url, reply_to_message_id=msg_id)
+        except Exception:
+            # if things went wrong
+            bot.sendMessage(chat_id=chat_id,
+                            text="There was a problem in the name you used, please enter different name",
+                            reply_to_message_id=msg_id)
 
-   return 'ok'
+    return 'ok'
+
 
 @app.route('/setwebhook', methods=['GET', 'POST'])
 def set_webhook():
@@ -69,9 +72,12 @@ def set_webhook():
     else:
         return "webhook setup failed"
 
+
 @app.route('/')
 def index():
     return '.'
+
+
 if __name__ == '__main__':
     # note the threaded arg which allow
     # your app to have more than one thread
