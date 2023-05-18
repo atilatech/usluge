bd24095c151496a7c7debcfe337fefa5174c0f21
Tomiwa Ademidun <tomiademidun@gmail.com>
2023-05-18 20:15:46 +0200

    full application flow

--- bot.py
+++ bot.py
@@ -1,12 +1,15 @@
 import logging
 
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters, PicklePersistence, \
+    CallbackQueryHandler
 
 from utils.credentials import BOT_TOKEN
 from utils.prompt import get_conversation_chain
 from utils.save_data import save_message_response
-from utils.taxi import find_taxi, get_driver_price
+from utils.taxi import find_taxi, get_driver_price, confirm_price_with_rider, get_matching_ride_request, \
+    update_ride_request_accept_ride
+from utils.utils import RIDE_REQUESTS_KEY
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -36,9 +39,17 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
     response = ''
     if update.message.text.startswith('taxi'):
-        await find_taxi(update.message.text, application.bot)
-    elif update.message.text.startswith('accept'):
-        await get_driver_price(update.effective_chat.id, context.bot)
+        await find_taxi(update, application.bot, context)
+    elif update.message.text.isdigit():
+        ride_request = get_matching_ride_request(str(update.effective_chat.id), context.bot_data)
+
+        rider_id = ride_request['rider']['id']
+        context.bot_data[RIDE_REQUESTS_KEY][str(rider_id)]['response'] = update.message.text
+        await confirm_price_with_rider(ride_request['request'],
+                                       update.message.text,
+                                       rider_id,
+                                       ride_request['driver']['first_name'],
+                                       context.bot)
     else:
         response = get_conversation_chain().predict(human_input=update.message.text)
         await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
@@ -46,13 +57,68 @@ async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
     save_message_response(response, update.message)
 
 
+async def accept_ride(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    cqd = update.callback_query.data
+    # message_id = update.callback_query.message.message_id
+    # update_id = update.update_id
+    if cqd.startswith('accept__'):
+        requester_user_id = cqd.split('__')[1]
+        driver = update.callback_query.from_user
+        text = f'Your ride has been accepted by {driver.first_name}.\nWaiting for them to send their price.'
+        print('requester_user_id, driver', requester_user_id, driver)
+        update_ride_request_accept_ride(requester_user_id, driver, context)
+        print('context.bot_data', context.bot_data)
+        await context.bot.send_message(chat_id=requester_user_id, text=text)
+        await get_driver_price(driver.id, context.bot)
+
+    if cqd.startswith('accept_offer'):
+        rider = update.callback_query.from_user
+        ride_request = context.bot_data[RIDE_REQUESTS_KEY][str(rider.id)]
+
+        message_template = "Taxi Confirmed! {first_name} (@{username}) will message " \
+                           "you shortly to arrange a pickup.\n\n" \
+                           "Details: {request} \n" \
+                           "Price: {response} Euros \n" \
+                           'You can also send them a message: https://t.me/{username}.\n' \
+                           # 'Tip: Add @uslugebot to your chat with {first_name} to instantly add trip details and
+        # price.'
+
+        rider_message = message_template.format(first_name=ride_request['driver']['first_name'],
+                                                username=ride_request['driver']['username'],
+                                                request=ride_request['request'],
+                                                response=ride_request['response'])
+
+        driver_message = message_template.format(first_name=ride_request['rider']['first_name'],
+                                                 username=ride_request['rider']['username'],
+                                                 request=ride_request['request'],
+                                                 response=ride_request['response'])
+
+        print('rider_message, driver_message', rider_message, driver_message)
+        await context.bot.send_message(chat_id=ride_request['rider']['id'], text=rider_message)
+        await context.bot.send_message(chat_id=ride_request['driver']['id'], text=driver_message)
+
+    if cqd.startswith('decline_offer'):
+        rider = update.callback_query.from_user
+        ride_request = context.bot_data[RIDE_REQUESTS_KEY][str(rider.id)]
+
+        text = "Offer was declined"
+
+        await context.bot.send_message(chat_id=ride_request['rider']['id'], text=text)
+        await context.bot.send_message(chat_id=ride_request['driver']['id'], text=text)
+        context.bot_data[RIDE_REQUESTS_KEY][str(rider.id)]['driver'] = None
+
+
 if __name__ == '__main__':
-    application = ApplicationBuilder().token(BOT_TOKEN).build()
+    persistence = PicklePersistence(filepath="bot_data")
+    application = ApplicationBuilder().token(BOT_TOKEN).persistence(persistence).build()
 
     start_handler = CommandHandler('start', start)
     chat_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), chat)
 
+    accept_ride_handler = CallbackQueryHandler(accept_ride)
+
     application.add_handler(start_handler)
     application.add_handler(chat_handler)
+    application.add_handler(accept_ride_handler)
 
     application.run_polling()
--- utils/taxi.py
+++ utils/taxi.py
@@ -1,32 +1,111 @@
 import telegram
-from telegram import Bot
+from telegram import Bot, Update
+from telegram.ext import ContextTypes
+
+from utils.utils import get_random_string, RIDE_REQUESTS_KEY
 
 drivers = [
+    # {
+    #     'username': 'IvanKapisoda',
+    #     'first_name': 'Ivan',
+    #     'id': '1642664602',
+    # },
     {
-        'username': 'IvanKapisoda',
-        'first_name': 'Ivan',
-        'user_id': '1642664602',
+        'username': 'Tomiwa',
+        'first_name': 'tomiwa1a1',
+        'id': '5238299107',
     },
 ]
 
 
-async def find_taxi(request, bot: Bot):
+def get_matching_ride_request(target_driver_id, bot_data):
+    ride_requests = bot_data[RIDE_REQUESTS_KEY]
+    for rider_id, ride_request in ride_requests.items():
+        if ride_request['driver'] is not None:
+            driver_id = ride_request['driver']['id']
+            if driver_id == target_driver_id:
+                return ride_request
+
+    return None
+
+
+def create_ride_request(context: ContextTypes.DEFAULT_TYPE, rider: telegram.User, text: str):
+    ride_request = {
+        'id': get_random_string(),
+        'rider': {
+            'first_name': rider.first_name,
+            'username': rider.username,
+            'id': str(rider.id),
+        },
+        'driver': None,
+        'request': text,
+        'response': None,
+    }
+    if RIDE_REQUESTS_KEY not in context.bot_data:
+        context.bot_data[RIDE_REQUESTS_KEY] = {}
+
+    context.bot_data[RIDE_REQUESTS_KEY][str(rider.id)] = ride_request
+
+    return context.bot_data[RIDE_REQUESTS_KEY]
+
+
+def update_ride_request_accept_ride(rider_id, driver: telegram.User, context: ContextTypes.DEFAULT_TYPE):
+    driver = {
+        'first_name': driver.first_name,
+        'username': driver.username,
+        'id': str(driver.id),
+    }
+
+    context.bot_data[RIDE_REQUESTS_KEY][rider_id]['driver'] = driver
+
+    return context.bot_data[RIDE_REQUESTS_KEY]
+
+
+async def find_taxi(update: Update, bot: Bot, context: ContextTypes.DEFAULT_TYPE):
+    request = update.message.text
+    create_ride_request(context, update.message.from_user, update.message.text)
     for driver in drivers:
         print('update.message', request)
-        custom_keyboard = [['accept ✅', 'decline 🚫']]
-        reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard, one_time_keyboard=True)
-        await bot.send_message(
-            chat_id=driver['user_id'],
-            text=f"Taxi Request: {request}",
-            reply_markup=reply_markup
+        print('context.bot_data', context.bot_data)
+        accept_callback_data = f"accept__{update.message.from_user.id}"
+        decline_callback_data = f"decline__{update.message.from_user.id}"
+        accept_button = telegram.InlineKeyboardButton(
+            text='accept ✅',  # text that's shown to the user
+            callback_data=accept_callback_data  # text send to the bot when user taps the button
         )
-        await bot.send_message(chat_id=driver['user_id'], text=f'Rider Requesting a taxi: {request}')
+        decline_button = telegram.InlineKeyboardButton(
+            text='decline 🚫',  # text that's shown to the user
+            callback_data=decline_callback_data  # text send to the bot when user taps the button
+        )
+        await bot.send_message(
+            chat_id=driver['id'],
+            text=f"New Request: {request}",
+            reply_markup=telegram.InlineKeyboardMarkup([[accept_button, decline_button]])
+        )
 
 
 async def get_driver_price(chat_id, bot: Bot):
-
     # Send the message with the number pad keyboard
     await bot.send_message(
         chat_id=chat_id,
-        text="How much do you charge?",
+        text="How much do you charge for this trip in Euros?",
+    )
+
+
+async def confirm_price_with_rider(request, amount, rider_id, driver_name, bot):
+    accept_callback_data = f"accept_offer"
+    decline_callback_data = f"decline_offer"
+
+    accept_button = telegram.InlineKeyboardButton(
+        text='accept ✅',  # text that's shown to the user
+        callback_data=accept_callback_data  # text send to the bot when user taps the button
+    )
+    decline_button = telegram.InlineKeyboardButton(
+        text='decline 🚫',  # text that's shown to the user
+        callback_data=decline_callback_data  # text send to the bot when user taps the button
+    )
+    await bot.send_message(
+        chat_id=rider_id,
+        text=f"Taxi Offer from {driver_name}: {amount} euros for '{request}'",
+        reply_markup=telegram.InlineKeyboardMarkup([[accept_button, decline_button]])
     )
